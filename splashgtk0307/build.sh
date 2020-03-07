a22536cf224b4a8ec868abaf7a50f9a629996aa2
sykazcr <40899235+sykazcr@users.noreply.github.com>
2020-03-07 13:53:57 +0900

    add main window

--- splashgtk0307/build.sh
+++ splashgtk0307/build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-g++ gggtkmmsplash.cpp -g3 -O0 -o view.elf `pkg-config gtkmm-3.0 --cflags --libs`
+echo g++ gggtkmmsplash.cpp -Wall -g3 -O0 -o view.elf `pkg-config gtkmm-3.0 --cflags --libs`
+
+g++ gggtkmmsplash.cpp -Wall -g3 -O0 -o view.elf `pkg-config gtkmm-3.0 --cflags --libs`
 
--- splashgtk0307/gggtkmmsplash.cpp
+++ splashgtk0307/gggtkmmsplash.cpp
@@ -1,16 +1,77 @@
 // sykazcr
+#include <cstdio>
 #include <gtkmm.h>
 #include <string>
 #include <iostream>
 #include <glibmm/convert.h>
 
+class MainWindow : public Gtk::Window
+{
 
+public:
+	MainWindow();
+	virtual ~MainWindow(){};
+private:
+    Gtk::Button m_button1, m_button2;
+    Gtk::HBox m_hbox;
+    //Gtk::Tooltip m_tooltip;
+};
+
+MainWindow::MainWindow()
+    : m_button1( "button1" ),
+      m_button2( "button2" )
+{
+    m_hbox.pack_start( m_button1 );
+    m_hbox.pack_start( m_button2 );
+
+    //m_tooltip.set_text( m_button1, Glib::ustring("button1") );
+    //m_tooltip.set_text( m_button2, Glib::ustring("button2") );
+
+    add( m_hbox );
+    show_all_children();
+}
+
+
+//-
+//---
+//-----
+//--*** A P P *****************************************************************
+//-----
+//---
+//-
+class App : public Gtk::Main {
+ 
+public:
+
+	App(const std::string& dir, int *argc, char ***argv);
+	virtual ~App(){};
+
+    MainWindow         *main_window;
+};
+
+App::App(const std::string& dir, int *argc, char ***argv) 
+{
+	main_window = new MainWindow();
+	main_window->set_default_size(300, 450);
+    main_window->present();
+}
+
+
+//MainWindow         *App::main_window    = NULL;
+
+//-
+//---
+//-----
+//--*** M A I N *****************************************************************
+//-----
+//---
+//-
 int main(int argc, char **argv)
 {
 
 	try
 	{
-		App app(etl::dirname(binary_path), &argc, &argv);
+		App app(std::string("/home/work/splash/"), &argc, &argv);
 
 		app.run();
 	}
